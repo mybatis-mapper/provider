82ca7be17a77f41e3295dac5f764e027802e2404
liuzh <abel533@gmail.com>
2024-05-19 12:30:05 +0800

    user和username都容易和环境变量冲突，直接去掉

--- src/test/java/io/mybatis/provider/config/ConfigHelperTest.java
+++ src/test/java/io/mybatis/provider/config/ConfigHelperTest.java
@@ -26,8 +26,6 @@ public class ConfigHelperTest {
   @Test
   public void testUserConfig() {
     Config config = new ProviderUserConfig();
-    String username = config.getStr("user");
-    Assert.assertEquals("liuzh", username);
     Assert.assertEquals("abel533@gmail.com", config.getStr("email"));
   }
 
@@ -48,7 +46,6 @@ public class ConfigHelperTest {
 
   @Test
   public void test() {
-    Assert.assertEquals("liuzh", ConfigHelper.getStr("user"));
     Assert.assertEquals("abel533@gmail.com", ConfigHelper.getStr("email"));
     Assert.assertEquals("upper_underscore", ConfigHelper.getStr("mybatis.provider.style"));
   }
